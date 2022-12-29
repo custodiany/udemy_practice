44af0974665f399fc5bafff33952381b864b1fa4
kerberos <hyeongseokgim@gmail.com>
2022-12-29 22:39:46 +0900

    실습1. 인사말과 사용자에게 입력받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>) {
     println("이름을 넣어주세요!")
     val name = readLine()
-    println("안녕하세요 $name 님! 반갑습니다.")
+    println("""
+        감사합니다. 반갑습니다, $name 님.
+        원하시는 카테고리를 입력해주세요.
+    """.trimIndent())
 }
